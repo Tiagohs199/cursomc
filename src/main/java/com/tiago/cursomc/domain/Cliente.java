d5bb9259e1b61f69a497ce85549d6ab2f1d1b119
Tiago Silva <tiagohs.199@gmail.com>
2021-07-26 21:40:16 -0300

    EndPoint /clientes/id available

--- src/main/java/com/tiago/cursomc/domain/Cliente.java
+++ src/main/java/com/tiago/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.tiago.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -29,8 +30,10 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
+	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
