2b3f468a96b4aaecd142fc8d278bc2325e880f19
Tiago Silva <tiagohs.199@gmail.com>
2021-07-29 21:28:55 -0300

    EndPoint /pedidos/{id} available

---
+++ src/main/java/com/tiago/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.tiago.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiago.cursomc.domain.Cliente;
+import com.tiago.cursomc.domain.Pedido;
+import com.tiago.cursomc.repositories.PedidoRepository;
+import com.tiago.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido findById(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! ID: "+ id +", Tipo: "+ Pedido.class.getName()));
+	}
+}
