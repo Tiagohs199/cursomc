e3c569d71209aa42cd9b5444a2ac44a7733915e0
Tiago Silva <tiagohs.199@gmail.com>
2021-08-21 21:45:29 -0300

    validação sintatica com bean javax validation

--- src/main/java/com/tiago/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/tiago/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.tiago.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.tiago.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	@NotEmpty(message = "Peenchimento obrigatorio")
+	@Length(min = 5,max= 80, message ="O tamanho deve ser entre 5 e 80 caracteres" )
 	private String name;
 	
 	public CategoriaDTO() {
