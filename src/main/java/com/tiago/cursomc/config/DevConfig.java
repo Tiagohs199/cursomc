38d27cb31729b979ae902cfebe53d34c504a0b11
Tiago Silva <tiagohs.199@gmail.com>
2021-09-26 20:58:08 -0300

    criando profile de dev

---
+++ src/main/java/com/tiago/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.tiago.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.tiago.cursomc.services.DbService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DbService db;
+	
+	@Value("$spring.jpa.hibernate.ddl.auto")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws Exception {
+		
+		if (! "create".equals(strategy)) {
+			return false;
+		}
+		
+		db.instantiateTestDatabase();
+		
+		return true;
+	}
+
+}
