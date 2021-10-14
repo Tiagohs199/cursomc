8232abd8626b140206366267be7be696370a9140
Tiago Silva <tiagohs.199@gmail.com>
2021-10-14 11:42:40 -0300

    esqueci a senha

---
+++ src/main/java/com/tiago/cursomc/dto/EmailDTO.java
@@ -0,0 +1,26 @@
+package com.tiago.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+	
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Email(message = "Email invalido")
+	public EmailDTO() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
